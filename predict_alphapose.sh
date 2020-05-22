9fc4cd79b1de4a7da26ba919b0d30b5bd9863c2f
Egor Nemchinov <immortalsuperturtle@gmail.com>
2020-05-22 15:46:33 +0300

    Fix exit commands in script; fix helper script path

--- predict_alphapose.sh
+++ predict_alphapose.sh
@@ -15,12 +15,14 @@ elif [ -f $d/source.mp4 ]; then
     VID_PATH=$d/source.mp4
 elif [ -d $d/images ]; then
     VID_PATH=$d/`basename $d`.mp4
-    ffmpeg -i $d/images/%05d.png -pix_fmt yuv420p -c:v libx264 $VID_PATH || \
-    ffmpeg -i $d/images/%04d.png -pix_fmt yuv420p -c:v libx264 $VID_PATH || \
-    ffmpeg -i $d/images/%05d_img.png -pix_fmt yuv420p -c:v libx264 $VID_PATH || \
-    ffmpeg -i $d/images/%04d_img.png -pix_fmt yuv420p -c:v libx264 $VID_PATH || \
-    printf "Couldn't figure out formatting of images in $d/images folder\nConvert yourself to $d/`basename $d`.mp4" && \
-    exit 1
+    ffmpeg -i $d/images/%05d.png -pix_fmt yuv420p -c:v libx264 $VID_PATH -loglevel panic || \
+    ffmpeg -i $d/images/%04d.png -pix_fmt yuv420p -c:v libx264 $VID_PATH -loglevel panic || \
+    ffmpeg -i $d/images/%05d_img.png -pix_fmt yuv420p -c:v libx264 $VID_PATH -loglevel panic || \
+    ffmpeg -i $d/images/%04d_img.png -pix_fmt yuv420p -c:v libx264 $VID_PATH -loglevel panic
+    if [ $? -ne 0 ]; then
+      printf "Couldn't figure out formatting of images in $d/images folder\nConvert yourself to $d/`basename $d`.mp4"
+      exit 1
+    fi
 else
     echo "Directory $d must contain either source.mp4/"`basename $d`.mp4" or folder 'images'"
     exit 1
@@ -32,8 +34,11 @@ CUR_DIR=$PWD
 cd $ALPHAPOSE_DIR
 ./scripts/inference.sh configs/coco/resnet/256x192_res152_lr1e-3_1x-duc.yaml \
     pretrained_models/fast_421_res152_256x192.pth \
-    $VID_PATH $d/alphapose_results || \
-    echo "Failed to run alphapose.." && exit 1
+    $VID_PATH $d/alphapose_results
+if [ $? -ne 0 ]; then
+  echo "Failed to run alphapose.."
+  exit 1
+fi
 cd $CUR_DIR
 
 echo "--> Converting AlphaPose to OpenPose format"
@@ -41,4 +46,4 @@ if [ ! -d $d/images ]; then
     echo "To convert to keypoints, unpack video into frames into folder $d/images"
     exit 1
 fi
-python utils/format_alphapose_kpts.py $d/alphapose_results $d/keypoints --frames_dir $d/images
+python scripts/format_alphapose_kpts.py $d/alphapose_results/alphapose-results.json $d/keypoints --frames_dir $d/images
