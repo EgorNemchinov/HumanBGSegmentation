b1d0c454614b4e5d3e87fd74427283747bd79aa4
Egor Nemchinov <immortalsuperturtle@gmail.com>
2020-05-30 19:11:12 +0000

    Add arguments to extract_bg.py for skipping frames in the beginning and the end

--- extract_bg.py
+++ extract_bg.py
@@ -5,8 +5,10 @@ import argparse
 import os
 
 
-def load_img_mask(img_dir, index):
+def load_img_mask(img_dir, index, skip_last=0):
     files = sorted([f for f in os.listdir(img_dir) if f.endswith('_img.png')])
+    if skip_last > 0:
+        files = files[:-skip_last]
     if index >= len(files):
         return None, None
     filename = files[index]
@@ -17,15 +19,15 @@ def load_img_mask(img_dir, index):
     return img, mask[:, :, np.newaxis]
 
 
-def main(img_dir, bg_out_path, step=8, inpaint=True):
-    i = 1
+def main(img_dir, bg_out_path, step=8, inpaint=True, skip_first=0, skip_last=0):
+    i = 1 + skip_first
     img, mask = load_img_mask(img_dir, i)
     frame_sum, mask_sum = 0, 0
     while (img is not None) and (mask is not None):
         frame_sum = frame_sum + img * (1 - mask / 255.)
         mask_sum = mask_sum + (1 - mask / 255.)
         i += step
-        img, mask = load_img_mask(img_dir, i)
+        img, mask = load_img_mask(img_dir, i, skip_last=skip_last)
     print(f'Total images used for extracting bg: {i//step}')
     coords = np.argwhere(mask_sum > 0)
     frame_sum[coords[:, 0], coords[:, 1], :] = frame_sum[coords[:, 0], coords[:, 1], :] / mask_sum[coords[:, 0], coords[:, 1]]
@@ -43,6 +45,8 @@ if __name__ == '__main__':
     parser.add_argument('bg_out_path')
     parser.add_argument('--step', type=int, default=8)
     parser.add_argument('--no_inpainting', action='store_true')
+    parser.add_argument('--skip_first', type=int, default=0, help='Exclude first N frames')
+    parser.add_argument('--skip_last', type=int, default=0, help='Exclude last N frames')
     args = parser.parse_args()
 
-    main(args.img_dir, args.bg_out_path, args.step, inpaint=(not args.no_inpainting))
+    main(args.img_dir, args.bg_out_path, args.step, inpaint=(not args.no_inpainting), skip_first=args.skip_first, skip_last=args.skip_last)
